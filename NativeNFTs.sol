53f6c70b33c1385640a016eaf94c895cee27c461
cjj03nes <cjj03nes@gmail.com>
2025-02-12 11:12:39 -0600

    Update NativeNFTs.sol

--- NativeNFTs.sol
+++ NativeNFTs.sol
@@ -1,1 +1,33 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
 
+import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFT is ERC1155, Ownable {
+    constructor(address initialOwner)
+        ERC1155("https://nftURIhere")
+        Ownable(initialOwner)
+    {}
+
+    function mint(address account, uint256 id, uint256 amount, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mintBatch(to, ids, amounts, data);
+    }
+
+   function buy(){}
+   function sell(){}
+   function auctionOff(){}
+   function placeAuctionBid(){}
+   function buyNowFromAuction(){}
+}
