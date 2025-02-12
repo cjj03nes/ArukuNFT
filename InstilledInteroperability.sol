4fad10f7c7e83f0f0f73844d396f3adc776941bc
cjj03nes <cjj03nes@gmail.com>
2025-02-12 10:50:45 -0600

    Update InstilledInteroperability.sol

--- InstilledInteroperability.sol
+++ InstilledInteroperability.sol
@@ -1,3 +1,174 @@
-pragma solidity ^0.8;
+
+pragma solidity > ^ 0.8;
+
+//Stellar & VerififiedTokens on that chain
+// Base & VerififiedTokens on that chain
+// Ethereum & VerififiedTokens on that chain
+
+
+
+ //  An aggregate of Rpc URLs came from https://chainlist.org/ 
+ // If yall want to add more chains, just follow the syntax for VerifiedBlockchains
+ // Im going to add generic tokens but yall can follow the syntax for VerifiedTokenAssets if yall want to add / remove any more 
+// The crowdfund contract is going to be made on top of crossChainTransfers && crossChainSwaps
+
+
+
+
+contract InstilledInteroperability ={
+
+VerifiedBlockchain ={
+
+    [1. ethereumBlockchain ],={
+        chainName = Ethereum;
+        chainId =  1;
+        chainRpcUrl1 = https://eth.llamarpc.com ;
+        chainRpcUrl2 = https://eth.blockrazor.xyz ;
+        chainRpcUrl3 = https://ethereum-rpc.publicnode.com ;
+        chainRpcUrl4 = https://eth-pokt.nodies.app ;
+        chainRpcUrl5 = https://eth.meowrpc.com ;
+        chainCurrency=  ETH;
+        returns Ethereum Mainnet;
+
+
+        ethVerifiedTokenAssets ={
+
+
+    USD Tether = [
+            tokenAssetName = United States Dollar Tether;
+            tokenAssetDecimal =  6;
+            tokenAssetSymbol = USDT;
+            tokenAssetContractAddress = 0xdAC17F958D2ee523a2206206994597C13D831ec7;],
+
+    USD Coin  = [
+        tokenAssetName = United States Dollar Coin;
+        tokenAssetDecimal =  6;
+        tokenAssetSymbol = USDC;
+        tokenAssetContractAddress = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; ],  
+        
+    Wrapped Ethereum  = [
+        tokenAssetName = Wrapped Ethereum;
+        tokenAssetDecimal =  18;
+        tokenAssetSymbol = wETH;
+        tokenAssetContractAddress =  0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2; ],
+        
+    Binance  = [
+        tokenAssetName = Binance;
+        tokenAssetDecimal = 18; 
+        tokenAssetSymbol = BNB;
+        tokenAssetContractAddress = 0xB8c77482e45F1F44dE1745F52C74426C631bDD52; ],
+        
+    Matic  = [
+        tokenAssetName = Matic Token ;
+        tokenAssetDecimal = 18; 
+        tokenAssetSymbol = MATIC;
+        tokenAssetContractAddress = 0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0;        ],
+        
+    Chainlink  = [
+        tokenAssetName = Chainlink;
+        tokenAssetDecimal =  18;
+        tokenAssetSymbol = LINK;
+        tokenAssetContractAddress = 0x514910771AF9Ca656af840dff83E8264EcF986CA; ]     }
+    }
+
+
+
+
+
+    [2. baseBlockchain ], ={
+        chainName = Base Mainnet ;
+        chainId = 8453 ;
+        chainRpcUrl1 = https://base-rpc.publicnode.com ;
+        chainRpcUrl2 = https://base.meowrpc.com;
+        chainRpcUrl3 =  wss://base.callstaticrpc.com ;
+        chainRpcUrl4 =  https://base-pokt.nodies.app ;
+        chainRpcUrl5 =  https://0xrpc.io/base;
+        chainCurrency=  ETH ;
+        returns Base Mainnet;
+
+        baseVerifiedTokenAssets ={
+/*
+    USD Tether = [
+            tokenAssetName = United States Dollar Tether;
+            tokenAssetDecimal =  6;
+            tokenAssetSymbol = USDT;
+            tokenAssetContractAddress = 0x55d398326f99059fF775485246999027B3197955; ],
+
+    USD Coin  = [
+        tokenAssetName = United States Dollar Coin;
+        tokenAssetDecimal =  6;
+        tokenAssetSymbol = USDC;
+        tokenAssetContractAddress =  0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d; ],  
+        
+     Ethereum  = [
+        tokenAssetName =  Ethereum;
+        tokenAssetDecimal =  18;
+        tokenAssetSymbol = ETH;
+        tokenAssetContractAddress =  0x2170Ed0880ac9A755fd29B2688956BD959F933F8; ],
+        
+    Wrapped Binance  = [
+        tokenAssetName = Wrapped Binance;
+        tokenAssetDecimal = 18; 
+        tokenAssetSymbol = wBNB;
+        tokenAssetContractAddress =  0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c; ],
+        
+    Matic  = [
+        tokenAssetName = Matic Token ;
+        tokenAssetDecimal = 18; 
+        tokenAssetSymbol = MATIC;
+        tokenAssetContractAddress =  0xCC42724C6683B7E57334c4E856f4c9965ED682bD;        ],
+        
+    Chainlink  = [
+        tokenAssetName = Chainlink;
+        tokenAssetDecimal =  18;
+        tokenAssetSymbol = LINK;
+        tokenAssetContractAddress =  0xF8A0BF9cF54Bb92F17374d9e9A321E6a111a51bD; ]  */
+
+        }
+    }
+
+
+
+
+
+
+          
+     }
+
+
+
+// declaring chains => tokens
+mapping(ethereumBlockchain => uint256) ethVerifiedTokenAsset;
+mapping(binanceBlockchain => uint256)  bnbVerifiedTokenAsset;
+mapping(polygonBlockchain => uint256)  polyVerifiedTokenAsset;
+
+
+
+//eth tokens => bnb tokens
+if ethVerifiedTokenAsset(tokenAssetName, tokenAssetDecimal, tokenAssetSymbol) == bnbVerifiedTokenAsset(tokenAssetName, tokenAssetDecimal, tokenAssetSymbol) then return crossChainTransfer,
+else if ethVerifiedTokenAsset(tokenAssetName, tokenAssetDecimal, tokenAssetSymbol) != bnbVerifiedTokenAsset(tokenAssetName, tokenAssetDecimal, tokenAssetSymbol) then return crossChainSwap;
+// eth tokens => polygon tokens
+if ethVerifiedTokenAsset(tokenAssetName, tokenAssetDecimal, tokenAssetSymbol) == polyVerifiedTokenAsset(tokenAssetName, tokenAssetDecimal, tokenAssetSymbol) then return crossChainTransfer,
+else if ethVerifiedTokenAsset(tokenAssetName, tokenAssetDecimal, tokenAssetSymbol) != polyVerifiedTokenAsset(tokenAssetName, tokenAssetDecimal, tokenAssetSymbol) then return crossChainSwap;
+
+
+// bnb token => eth tokens
+if bnbVerifiedTokenAsset(tokenAssetName, tokenAssetDecimal, tokenAssetSymbol) == ethVerifiedTokenAsset(tokenAssetName, tokenAssetDecimal, tokenAssetSymbol) then return crossChainTransfer,
+else if bnbVerifiedTokenAsset(tokenAssetName, tokenAssetDecimal, tokenAssetSymbol) != ethVerifiedTokenAsset(tokenAssetName, tokenAssetDecimal, tokenAssetSymbol) then return crossChainSwap;
+//bnb => polygon tokens
+if bnbVerifiedTokenAsset(tokenAssetName, tokenAssetDecimal, tokenAssetSymbol) == polyVerifiedTokenAsset(tokenAssetName, tokenAssetDecimal, tokenAssetSymbol) then return crossChainTransfer,
+else if bnbVerifiedTokenAsset(tokenAssetName, tokenAssetDecimal, tokenAssetSymbol) != polyVerifiedTokenAsset(tokenAssetName, tokenAssetDecimal, tokenAssetSymbol) then return crossChainSwap;
+
+
+ //polygon tokens => eth token
+ if polyVerifiedTokenAsset(tokenAssetName, tokenAssetDecimal, tokenAssetSymbol) == ethVerifiedTokenAsset(tokenAssetName, tokenAssetDecimal, tokenAssetSymbol) then return crossChainTransfer,
+ else if polyVerifiedTokenAsset(tokenAssetName, tokenAssetDecimal, tokenAssetSymbol) != ethVerifiedTokenAsset(tokenAssetName, tokenAssetDecimal, tokenAssetSymbol) then return crossChainSwap;   
+//polygon tokens => bnb tokens
+if polyVerifiedTokenAsset(tokenAssetName, tokenAssetDecimal, tokenAssetSymbol) == bnbVerifiedTokenAsset(tokenAssetName, tokenAssetDecimal, tokenAssetSymbol) then return crossChainTransfer,
+else if polyVerifiedTokenAsset(tokenAssetName, tokenAssetDecimal, tokenAssetSymbol) != bnbVerifiedTokenAsset(tokenAssetName, tokenAssetDecimal, tokenAssetSymbol) then return crossChainSwap;
+}
+
+
+return default InstilledInteroperability;
 
 
